09866797afb00f16e7805d3425ed8bc6af12dce1
ramesh <rameshnagaraju@gmail.com>
2021-05-16 12:24:23 +0530

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,22 +1,39 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { RouterModule } from '@angular/router';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
 import { HomeComponent } from './home/home.component';
+import { DefaultComponent } from './default/default.component';
+import { FeaturesComponent } from './features/features.component';
+import { AboutusComponent } from './aboutus/aboutus.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
     FooterComponent,
-    HomeComponent
+    HomeComponent,
+    DefaultComponent,
+    FeaturesComponent,
+    AboutusComponent,
+    PageNotFoundComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    RouterModule.forRoot([
+      { path: 'home', component: HomeComponent },
+      { path: 'features', component: FeaturesComponent },
+      { path: 'aboutus', component: AboutusComponent },
+      { path: '', redirectTo: 'home', pathMatch: 'full' },
+      { path: '**', component: PageNotFoundComponent }
+    ]),
   ],
   providers: [],
   bootstrap: [AppComponent]
